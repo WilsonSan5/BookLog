5f7c69e51909a913790bbdf2692649bf8b13b9a3
Wilson <wilson.san57@gmail.com>
2025-06-12 15:21:37 +0200

    fetch all books done

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
-console.log('NTM');
+const ApiRoute = "https://keligmartin.github.io/api/books.json";
+
+const toReadColumn = document.getElementById('toRead');
+ 
+
+let allBooks = [];
+
+async function getAllBooks() {
+  await fetch(ApiRoute)
+    .then((response) => {
+      console.log(response);
+      return response.json();
+    })
+    .then((data) => {
+      allBooks = data;
+      console.log(allBooks);
+    })
+    .catch((error) => {
+      console.error("Erreur lors de la récupération des books :", error);
+    });
+}
+
+getAllBooks();
 
